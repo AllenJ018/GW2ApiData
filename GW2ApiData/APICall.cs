f459be39e091baf43230a8db8dfb9d6f2a1910e5
AllenJ018 <89491381+AllenJ018@users.noreply.github.com>
2022-03-19 09:30:19 -0500

    Update APICall.cs

--- GW2ApiData/APICall.cs
+++ GW2ApiData/APICall.cs
@@ -11,7 +11,6 @@ namespace GW2ApiData
     class ApiCall
     {
         string passTo;
-        // = "FD6AE079-B9E3-F24D-B612-E136FDD5B1E2BE134C44-5140-430D-BEEA-A1FC7E91FE9E";
         string callToAPI = "api.guildwars2.com/v2/account?access_token=";
         
 
